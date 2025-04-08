df57673d9d9565b59feb90f7d0182bc5c0189ef2
Refqa <bashirrefqa@gmail.com>
2025-04-08 11:32:26 +0200

    Added models

---
+++ BPV_tool/BPV_tool.Server/Models/BPVApproval.cs
@@ -0,0 +1,18 @@
+﻿namespace BPV_app.Models
+{
+    public class BPVApproval
+    {
+        public int Id { get; set; }
+
+        public int BPVProcessId { get; set; }
+        public BPVProcess BPVProcess { get; set; }
+
+        public int ReviewerId { get; set; }
+        public User Reviewer { get; set; }
+
+        public string Status { get; set; }
+        public string? Comment { get; set; }
+        public DateTime ReviewedAt { get; set; }
+    }
+
+}
---
+++ BPV_tool/BPV_tool.Server/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+
+namespace BPV_app.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+
+        public ICollection<File> UploadedFiles { get; set; }
+        public ICollection<Feedback> GivenFeedback { get; set; }
+        public ICollection<BPVProcess> SupervisedProcesses { get; set; }
+        public ICollection<BPVProcess> StartedProcesses { get; set; }
+        public ICollection<BPVApproval> BPVApprovals { get; set; }
+        public ICollection<Log> Logs { get; set; }
+    }
+}
